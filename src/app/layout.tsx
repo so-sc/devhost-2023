9e444d79d7e44dce9e4fa8bd968c8039ed5a7751
srajan <kumarsrajan02@gmail.com>
2023-12-14 19:27:00 +0530

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,49 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "DevHost23",
-  description: "",
+  title: {
+    default: "DevHost 2023",
+    template: `%s - DevHost 2023`,
+  },
+  description: "SOSC's flagship student developer meet.",
+  keywords: [
+    "sosc",
+    "Sahyadri Open Source Community",
+    "sosc devhost",
+    "devhost 2023",
+    "DevHost",
+    "Devhost",
+    "Devhost 2023",
+    "DevHost 2023",
+  ],
+  creator: "so-sc",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://devhost.sosc.org.in/",
+    title: "DevHost 2023",
+    description: "SOSC's flagship student developer meet.",
+    siteName: "DevHost 2023",
+    images: [
+      {
+        url: "https://devhost.sosc.org.in/og.png",
+        width: 1200,
+        height: 630,
+        alt: "DevHost 2023",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "DevHost 2023",
+    description: "SOSC's flagship student developer meet.",
+    images: ["https://devhost.sosc.org.in/og.png"],
+    creator: "@sahyadri_osc",
+  },
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
 };
 
 export default function RootLayout({
